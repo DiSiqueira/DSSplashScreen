2d6d125d188e32e1f30725212ea5c7d7c3c3593a
Diego Martins de Siqueira <dieg0@live.com>
2016-05-22 17:19:36 -0300

    First commit

---
+++ SplashScreen/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SplashScreen
+{
+	[Activity(Label = "Splash!!!", Icon = "@mipmap/icon", Theme="@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			System.Threading.Thread.Sleep(3000); //Aguarda 3 segundos
+			this.StartActivity(typeof(MainActivity));
+			// Create your application here
+		}
+	}
+}
+
